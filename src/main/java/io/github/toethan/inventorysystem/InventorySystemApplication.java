a21780aec1c2ac51c88ac3210b3936caccf67a61
Ethan Wong <43189717+to-ethan@users.noreply.github.com>
2020-03-06 16:00:03 -0600

    Initial commit

---
+++ src/main/java/io/github/toethan/inventorysystem/InventorySystemApplication.java
@@ -0,0 +1,13 @@
+package io.github.toethan.inventorysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorySystemApplication.class, args);
+	}
+
+}
